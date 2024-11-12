d00902a37264f5c5937ace9b5009b902260bd4b9
gijohardent <s2200152@student.unklab.ac.id>
2024-11-13 02:45:11 +0800

    add images, add modules, update navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "text-reveal": "text-reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s",
+      },
+      keyframes: {
+        "text-reveal": {
+          "0%": {
+            transform: "translate(0, 100%)",
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+          },
+        },
+      },
+    },
     fontFamily: {
       display: "Helvetica",
       body: "sans-serif",
